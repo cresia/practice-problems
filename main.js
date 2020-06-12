288e68954d7189437ceffefdbb3f3c00fe9a6b30
KimKim Horas <kimkim4@gmail.com>
2020-06-11 19:25:38 -0700

    Completed mean-calculator

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log("hello");
+
+function meanCalculator(numbersArray){
+  var num = 0;
+
+  for(var i = 0; i < numbersArray.length; i++){
+    var mean = num+= numbersArray[i];
+    var meanResult = mean/numbersArray.length;
+  }
+
+  return meanResult;
+}
+
+console.log(meanCalculator([1,2,3,4,5]));
+console.log(meanCalculator([7,3,1,5,3,2]));
+console.log(meanCalculator([9,3,6]));
