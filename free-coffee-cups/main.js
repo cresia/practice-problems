1c469490ac58ed0622b6684967cb0958580a7847
KimKim Horas <kimkim4@gmail.com>
2020-06-23 20:41:57 -0700

    another way

--- free-coffee-cups/main.js
+++ free-coffee-cups/main.js
@@ -1,12 +1,21 @@
 
 function freeCoffeeCups(coffeeCups){
-  var freeCup = coffeeCups/6;
+  // var freeCup = coffeeCups/6;
+  // if (coffeeCups % 6 === 1 || coffeeCups % 6 === 0 ){
+  //   return coffeeCups + Math.floor(freeCup);
+  // } else {
+  //   return coffeeCups;
+  // }
 
-  if (coffeeCups % 6 === 1 || coffeeCups % 6 === 0 ){
-    return coffeeCups + Math.floor(freeCup);
-  } else {
-    return coffeeCups;
+
+  // ---- or -----
+  var freeCofee = 0;
+  for(var i = 1; i <= coffeeCups; i++){
+    if(i % 6 === 0){
+      freeCofee++
+    }
   }
+  return coffeeCups + freeCofee;
 }
 
 console.log(freeCoffeeCups(13));
