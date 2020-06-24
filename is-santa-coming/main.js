385cb5b7ccb49927a8343dafc8b644445a57837f
KimKim Horas <kimkim4@gmail.com>
2020-06-23 19:12:12 -0700

    another solution

--- is-santa-coming/main.js
+++ is-santa-coming/main.js
@@ -1,15 +1,20 @@
 
 function isSantaComing (dateObject){
 
-  var santaComing = new Date(2001, 11, 24);
-  var year = santaComing.getFullYear()
-  var month = santaComing.getMonth();
-  var date = santaComing.getDate();
-
-  if(dateObject.getFullYear() === year && dateObject.getMonth() === month && dateObject.getDate() === date) {
+  if(dateObject.getMonth() === 11 && dateObject.getDate() === 24){
     return true;
   }
   return false;
+
+  // var santaComing = new Date(2001, 11, 24);
+  // var year = santaComing.getFullYear()
+  // var month = santaComing.getMonth();
+  // var date = santaComing.getDate();
+
+  // if(dateObject.getFullYear() === year && dateObject.getMonth() === month && dateObject.getDate() === date) {
+  //   return true;
+  // }
+  // return false;
 }
 
 console.log("2001, 11, 24:", isSantaComing(new Date(2001, 11, 24))) // true
