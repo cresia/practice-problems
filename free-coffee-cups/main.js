59dc9c96ccfb184ade6f9d9a74e63e498e4bec93
KimKim Horas <kimkim4@gmail.com>
2020-06-23 20:18:17 -0700

    fix freeCoffeeCups

--- free-coffee-cups/main.js
+++ free-coffee-cups/main.js
@@ -1,20 +1,15 @@
 
 function freeCoffeeCups(coffeeCups){
-  var freeCup = 0;
+  var freeCup = coffeeCups/6;
 
-  if(coffeeCups % 6 === 1){
-    freeCup+=2;
-    return coffeeCups + freeCup;
-  } else if(coffeeCups % 6 === 0){
-    freeCup++;
-    return coffeeCups + freeCup;
-  }
-  else {
+  if (coffeeCups % 6 === 1 || coffeeCups % 6 === 0 ){
+    return coffeeCups + Math.floor(freeCup);
+  } else {
     return coffeeCups;
   }
-
 }
 
 console.log(freeCoffeeCups(13));
+console.log(freeCoffeeCups(18));
 console.log(freeCoffeeCups(6));
 console.log(freeCoffeeCups(5))
