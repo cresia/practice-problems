d8287a26e6936d7ead8915050ad9db62642ae691
KimKim Horas <kimkim4@gmail.com>
2020-07-11 13:39:49 -0700

    different solution

--- date-format/main.js
+++ date-format/main.js
@@ -12,6 +12,29 @@ function dateFormatter(date) {
 
 }
 
+//--------- different solution ----------
+// function dateFormatter(date) {
+//   var result = []
+//   var word = ''
+//   for (var i = 0; i <= date.length; i++) {
+//     if (date[i] === '/' || !date[i]) {
+//       result.push(word)
+//       word = ''
+//     } else {
+//       word += date[i]
+//     }
+//   }
+//   // We don't use the year variable in the code block because when it was
+//   // parsed as an integer all leading and trailing zeros were removed.
+//   var year = parseInt(result[2])
+//   if (year <= 20 && year >= 0) {
+//     result[2] = '20' + result[2]
+//   } else {
+//     result[2] = '19' + result[2]
+//   }
+//   return result[2] + result[1] + result[0]
+// }
+
 
 dateFormatter("05/22/46")// -> "19462205"
 dateFormatter("09/23/82") // -> "19822309"
